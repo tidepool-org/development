9c50f1ad5c786fc1b261669870e8046a52c3475d
Derrick Burns <derrick@tidepool.org>
2019-02-25 19:01:51 -0800

    Created install weave script.

---
+++ bin/install_weave.sh
@@ -0,0 +1,8 @@
+helm install --name flux \
+--set rbac.create=true \
+--set helmOperator.create=true \
+--set git.url=git@github.com:tidepool-org/dev-ops \
+--set git-poll-interval=1m \
+--set update-chart-deps=false \
+--namespace flux \
+weaveworks/flux
--- bin/upgrade_weave.sh
+++ bin/upgrade_weave.sh
@@ -1,7 +1,8 @@
-helm install --name flux \
+helm upgrade \
 --set rbac.create=true \
 --set helmOperator.create=true \
 --set git.url=git@github.com:tidepool-org/dev-ops \
 --set git-poll-interval=1m \
+--set update-chart-deps=false \
 --namespace flux \
-weaveworks/flux
+flux weaveworks/flux
