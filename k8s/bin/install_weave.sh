55a61f0578cc91a7107aa61e06e743dbf1993678
Derrick Burns <derrick@tidepool.org>
2019-03-08 16:35:37 -0800

    Put flux in default namespace.  Add option to not install crds.

--- k8s/bin/install_weave.sh
+++ k8s/bin/install_weave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VALUESFILE=${1:-values.yaml}
-helm install --namespace flux -f ${VALUESFILE} --repo https://weaveworks.github.io/flux flux
+helm install --name flux -f ${VALUESFILE} --repo https://weaveworks.github.io/flux flux
