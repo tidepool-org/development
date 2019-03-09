0a72bb3bef460a6d6c10fcbd75c16450b1cde877
Derrick Burns <derrick@tidepool.org>
2019-03-08 16:36:16 -0800

    Use flux in default namespace.

--- k8s/bin/upgrade_weave.sh
+++ k8s/bin/upgrade_weave.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-helm upgrade --namespace flux -f values-nocrd.yaml flux --repo https://weaveworks.github.io/flux flux
+helm upgrade -f values-nocrd.yaml flux --repo https://weaveworks.github.io/flux flux
