0234acc02335d8bf4a17d7a5cd5fb48d51f55b3a
Derrick Burns <derrickrburns@gmail.com>
2019-05-21 13:49:32 -0700

    Fixed issues with identify_loadbalancers and get_routes.py

--- bin/get_routes.py
+++ bin/get_routes.py
@@ -23,8 +23,8 @@ BASE_STRING = """
 hostedZoneId="Z2895YZY6K7CA5" #corresponds to tidepool.org
 aliasHostedZoneId="Z18D5FSROUN65G"
 
-elb=subprocess.check_output(["identify_loadbalancers"]).decode('utf-8')
-hosts=subprocess.check_output(["./virtual_hosts.sh"]).decode('utf-8').strip().split('\n')
+elb=subprocess.check_output(["identify_loadbalancers"]).decode('utf-8').strip()
+hosts=subprocess.check_output(["./virtual_hosts"]).decode('utf-8').strip().split('\n')
 
 changes = list()
 for virtual_host in hosts:
